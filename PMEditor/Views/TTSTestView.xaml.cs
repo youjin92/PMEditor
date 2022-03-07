671fe82100d23b3d96fccf8575075ad482f8a76c
fbwls123@naver.com <youjin92@github.com>
2022-03-08 07:14:13 +0900

    TTS테스트 모듈 추가

---
+++ PMEditor/Views/TTSTestView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PMEditor.Views
+{
+    /// <summary>
+    /// Interaction logic for TTSTestView
+    /// </summary>
+    public partial class TTSTestView : UserControl
+    {
+        public TTSTestView()
+        {
+            InitializeComponent();
+        }
+    }
+}
