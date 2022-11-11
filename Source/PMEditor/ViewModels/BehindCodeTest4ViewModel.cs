6fc7bb683606019742f12568691961ab311264f7
유진 <jyou@estsecurity.com>
2022-11-11 09:13:44 +0900

    feature/Add_TestSource

--- Source/PMEditor/ViewModels/BehindCodeTest4ViewModel.cs
+++ Source/PMEditor/ViewModels/BehindCodeTest4ViewModel.cs
@@ -52,14 +52,51 @@ namespace PMEditor.ViewModels
 
                 case "Input":
                     {
-                        DPText = Text2;
+                        DPText = "start";
                         break;
                     }
                 case "Test":
                     {
-                        string a = "012345";
-                        string b = $"{a[0]}{a[1]} {a[2]}{a[3]} {a[4]}{a[5]}";
-                        DPText = b;
+                        System.Windows.Controls.Button btn = null;
+                        System.Windows.Controls.Button btn1 = new System.Windows.Controls.Button();
+
+                        btn1.Content = "bbb";
+
+                        bool a = btn1?.Content?.ToString() == "bbb";
+                        bool b = btn?.Content?.ToString() == "bbb";
+                        bool c = null == "nnn";
+
+                        break;
+                    }
+                case "EventWaitHandle":
+                    {
+                        Task.Run(() => CheckLogin());
+                        break;
+                    }
+
+                case "Encryptor":
+                    {
+
+                        break;
+                    }
+
+                case "ForeachCallbyreference":
+                    {
+                        List<PersonB> intarray = new List<PersonB> { new PersonB { Name = "aaa", Age = 19 }, new PersonB { Name = "bbb", Age = 19 } };
+
+                        foreach (PersonB item in intarray)
+                        {
+                            if (item.Age == 19)
+                                item.Age = 20;
+                        }
+
+                        List<int> intarray2 = new List<int> { 1,2,3,4,5,6,7,8,9 };
+
+                        foreach (int item in intarray2)
+                        {
+
+                        }
+
                         break;
                     }
 
@@ -69,5 +106,31 @@ namespace PMEditor.ViewModels
             }
         }
 
+        private EventWaitHandle loginCheckEvent = null;
+        private void CheckLogin()
+        {
+            EventWaitHandle.TryOpenExisting("Test_EventWaitHandle", out loginCheckEvent);
+            if (loginCheckEvent != null)
+            {
+                while (true)
+                {
+                    Thread.Sleep(500);
+
+                    if (loginCheckEvent.IsSignaled())
+                    {
+                        DPText = "Signaled";
+                        break;
+                    }
+                }
+            }
+        }
+
+    }
+    public static class ExtEventWaitHandle
+    {
+        public static bool IsSignaled(this EventWaitHandle eventObj)
+        {
+            return (eventObj?.WaitOne(0) == true) ? true : false;
+        }
     }
 }
---
+++ Source/Test_Process/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test_Process
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private EventWaitHandle loginStateEvent = new EventWaitHandle(false, EventResetMode.ManualReset, "Test_EventWaitHandle");
+
+
+        private void Set_Button_Click(object sender, RoutedEventArgs e)
+        {
+            loginStateEvent.Set();
+        }
+
+        private void ReSet_Button_Click(object sender, RoutedEventArgs e)
+        {
+            loginStateEvent.Reset();
+        }
+    }
+}
