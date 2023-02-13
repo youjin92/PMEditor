668d84f3961794f47fdf0c5469c99e2cf2df9f12
DESKTOP-RK1EN2F\user <fbwls123@naver.com>
2023-02-14 00:25:52 +0900

    MYSQL Connection Insert Write Read Delete 예시 코드 작성

--- Source/PMEditor/ViewModels/BehindCodeTest5ViewModel.cs
+++ Source/PMEditor/ViewModels/BehindCodeTest5ViewModel.cs
@@ -23,13 +23,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Runtime.Caching;
 using Common;
+using MySql.Data.MySqlClient;
 
 namespace PMEditor.ViewModels
 {
     public class BehindCodeTest5ViewModel : BindableBase
     {
         public string Text { get; set; } = "TEST4";
-        
+        public string Name { get; set; } 
+        public int Age { get; set; }
+        public string WriteName { get; set; }
+        public int WriteAge { get; set; }
+        public string ReadName { get; set; }
+        public int ReadAge { get; set; }
+        public string DeleteName { get; set; }
+
+        MySqlConnection connection;
+
+
         public BehindCodeTest5ViewModel()
         {
         }
@@ -40,9 +51,121 @@ namespace PMEditor.ViewModels
 
             switch (param)
             {
-                case "GetDrive":
+                case "MySqlConnection":
                     {
- 
+                        connection = new MySqlConnection("Server=127.0.0.1;Database=member;Uid=root;Pwd=1q2w3e4r!!;");
+                        break;
+                    }
+
+                case "AddItem":
+                    {
+                        string insertQuery = $"INSERT INTO member_table(name,age) VALUES('{Name}', {Age})";
+
+                        connection.Open();
+                        MySqlCommand command = new MySqlCommand(insertQuery, connection);
+
+                        try//예외 처리
+                        {
+                            // 만약에 내가처리한 Mysql에 정상적으로 들어갔다면 메세지를 보여주라는 뜻이다
+                            if (command.ExecuteNonQuery() == 1)
+                            {
+                                MessageBox.Show("정상적으로 갔다");
+                            }
+                            else
+                            {
+                                MessageBox.Show("비정상 이당");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+
+
+                        connection.Close();
+                        break;
+
+                    }
+
+                case "Write":
+                    {
+                        connection.Open();
+                        string insertQuery = $"UPDATE member.member_table SET age={WriteAge} WHERE name='{WriteName}'";
+
+                        MySqlCommand command = new MySqlCommand(insertQuery, connection);
+
+                        try//예외 처리
+                        {
+                            // 만약에 내가처리한 Mysql에 정상적으로 들어갔다면 메세지를 보여주라는 뜻이다
+                            if (command.ExecuteNonQuery() == 1)
+                            {
+                                MessageBox.Show("정상적으로 갔다");
+                            }
+                            else
+                            {
+                                MessageBox.Show("비정상 이당");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        connection.Close();
+
+                        break;
+                    }
+
+                case "Read":
+                    {
+                        try
+                        {
+                            connection.Open();
+                            string sql = $"SELECT name, age FROM member.member_table WHERE name='{ReadName}'";
+
+                            MySqlCommand cmd = new MySqlCommand(sql, connection);
+                            MySqlDataReader rdr = cmd.ExecuteReader();
+
+                            while (rdr.Read())
+                            {
+                                Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                                ReadName = rdr[0].ToString();
+                                ReadAge = Convert.ToInt32( rdr[1] );
+                            }
+
+                            rdr.Close();
+                            connection.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.ToString());
+                        }
+                        break;
+                    }
+
+                case "Delete":
+                    {
+                        connection.Open();
+                        string insertQuery = $"DELETE FROM member.member_table WHERE name='{DeleteName}'";
+
+                        MySqlCommand command = new MySqlCommand(insertQuery, connection);
+
+                        try//예외 처리
+                        {
+                            // 만약에 내가처리한 Mysql에 정상적으로 들어갔다면 메세지를 보여주라는 뜻이다
+                            if (command.ExecuteNonQuery() == 1)
+                            {
+                                MessageBox.Show("정상적으로 갔다");
+                            }
+                            else
+                            {
+                                MessageBox.Show("비정상 이당");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        connection.Close();
                         break;
                     }
 
